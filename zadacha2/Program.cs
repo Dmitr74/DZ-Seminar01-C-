11ffc2685499603bb1a2b14ddb2c764bf10956f6
Dmitrii Lih <omts1dm@gmail.com>
2023-02-14 10:20:14 +0500

    вариант кода 02

--- zadacha2/Program.cs
+++ zadacha2/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Введите первое число: ");
+﻿Console.Clear.
+Console.Write("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
