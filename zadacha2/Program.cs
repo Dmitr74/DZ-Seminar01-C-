58f9d301a268699a8f56ba79be9a87e2ccf172f3
Dmitrii Lih <omts1dm@gmail.com>
2023-02-14 10:31:36 +0500

    вариант кода 04

--- zadacha2/Program.cs
+++ zadacha2/Program.cs
@@ -3,17 +3,19 @@ Console.Write("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
-if (number1 > Max)
+int maxnumber = -1;
+int minnumber = -1;
+if (number1 > maxnumber)
 {
-    Max = number1;
+    maxnumber = number1;
 }
-else if (number2 > Max)
+else if (number2 > maxnumber)
 {
-    Max = number2;
-    Max = number1;
+    maxnumber = number2;
+    maxnumber = number1;
 }
-else Max = number1;
-     Min = number2;
+else maxnumber = number1;
+     minnumber = number2;
 
 Console.Write(Max);
 Console.Write(Min);
