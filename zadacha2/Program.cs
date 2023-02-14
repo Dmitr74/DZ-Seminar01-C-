b3f0d9d8709fbabc86ed037a85c69ed3b63cba54
Dmitrii Lih <omts1dm@gmail.com>
2023-02-14 11:18:19 +0500

    вариант кода 15. добавил фигурные скобки и кирилицу

--- zadacha2/Program.cs
+++ zadacha2/Program.cs
@@ -3,15 +3,14 @@ Console.Write("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
-int maxnumber = number1;
 
 if (number1 > number2)
 {
-    Console.Write("maxnumber = number1" ); 
-    Console.Write("minnumber = number2" );
+    Console.Write("максимальное число = {number1}" ); 
+    Console.Write("минимальное число = {number2}" );
 }
 else if (number2 > number1)
 {
-    Console.WriteLine("maxnumber = number2" );
-    Console.WriteLine("minnumber = number1" );
+    Console.WriteLine("максимальное число = {number2}" );
+    Console.WriteLine("минимальное число = {number1}" );
 }
