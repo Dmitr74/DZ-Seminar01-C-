2c26f9eadacbbcd2621978f1d9be68b32f149760
Dmitrii Lih <omts1dm@gmail.com>
2023-02-14 11:48:04 +0500

    вариант 01

---
+++ zadacha4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"maxnumber = {number1}" ); 
+}
+else if (number2 > number3)
+{
+    Console.WriteLine($"maxnumber = {number2} " );
+}
+else if (number3 > number1)
+{
+    Console.WriteLine($"maxnumber = {number3} " );
+}
