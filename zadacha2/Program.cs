ac2322bdc8a8d7b3f64b962669effea477fd84e8
Dmitrii Lih <omts1dm@gmail.com>
2023-02-14 11:09:35 +0500

    вариант кода 12. убрал кавычки в скобках

--- zadacha2/Program.cs
+++ zadacha2/Program.cs
@@ -3,15 +3,15 @@ Console.Write("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
-int maxnumber = -1;
+int maxnumber = number1;
 
-if (number1 > maxnumber)
+if (number1 > number2)
 {
-    Console.Write("maxnumber = number1" ); 
-    Console.Write("minnumber = number2" );
+    Console.Write(maxnumber = number1 ); 
+    Console.Write(minnumber = number2 );
 }
 else if (number2 > number1)
 {
-    Console.Write("maxnumber = number2" );
-    Console.Write("minnumber = number1" );
+    Console.Write(maxnumber = number2 );
+    Console.Write(minnumber = number1 );
 }
