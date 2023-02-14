4d5ecc7af3d8764ed11485d96635709adef55f8f
Dmitrii Lih <omts1dm@gmail.com>
2023-02-14 10:14:14 +0500

    вариант кода 01

---
+++ zadacha2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > max)
+{
+    max = number1;
+}
+else if (number2 > max)
+{
+    max = number2,
+    min = number1;
+}
+else max = number1, min = number2;
+
+Console.Write("max ");
+Console.Write("min ");
+
+
