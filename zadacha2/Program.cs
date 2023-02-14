11fd97da72a52cab1f5093184b75fb40725603c1
Dmitrii Lih <omts1dm@gmail.com>
2023-02-14 10:38:34 +0500

    вариант кода 05

--- zadacha2/Program.cs
+++ zadacha2/Program.cs
@@ -14,8 +14,6 @@ else if (number2 > maxnumber)
     maxnumber = number2;
     maxnumber = number1;
 }
-else maxnumber = number1;
-     minnumber = number2;
 
 Console.Write(maxnumber);
 Console.WriteLine(minnumber);
