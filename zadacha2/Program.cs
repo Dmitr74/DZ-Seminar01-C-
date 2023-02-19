410e635758862ed0a81527a860b9a022d3cdb9c4
Dmitrii Lih <omts1dm@gmail.com>
2023-02-19 14:52:10 +0500

    задачи 2

--- zadacha2/Program.cs
+++ zadacha2/Program.cs
@@ -2,15 +2,15 @@
 Console.Write("Введите первое число: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
+int number22 = Convert.ToInt32(Console.ReadLine());
 
-if (number1 > number2)
+if (number1 > number22)
 {
     Console.WriteLine($"maxnumber = {number1}" ); 
-    Console.WriteLine($"minnumber = {number2}" );
+    Console.WriteLine($"minnumber = {number22}" );
 }
-else if (number2 > number1)
+else if (number22 > number1)
 {
-    Console.WriteLine($"maxnumber = {number2} " );
+    Console.WriteLine($"maxnumber = {number22} " );
     Console.WriteLine($"minnumber = {number1} " );
 }
