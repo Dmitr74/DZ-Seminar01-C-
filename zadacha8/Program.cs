dfb96fb33a826cc150494dba93e5ba196da5e75c
Dmitrii Lih <omts1dm@gmail.com>
2023-02-19 13:40:33 +0500

    задачи 2,4,6,8

---
+++ zadacha8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit = 0;
+
+while (digit < number)
+{
+    digit = digit + 1;
+    if (digit % 2 == 0)
+    Console.Write($"{digit} ");
+
+}
+ System.Console.WriteLine("Чётных чисел больше нет.");
